1f15154996275dfa96f232f7242ef39731214738
Chris <mr.christopher.hahn@gmail.com>
2026-03-06 21:32:46 -0500

    add styling and map

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 $(document).ready(function () {
-    // write your code here
+// download contents of data.json
+    $.getJSON('data.json', function(data){
 
-});
+        // iterate through the locations
+        $.each(data, function(index, spot) {
+
+            // pull coordinates from location
+            const lat = spot.location[0];
+            const lon = spot.location[1];
+
+            // construct map link
+            const mapUrl = `https://www.google.com/maps?q=${lat},${lon}`;
+
+            const row = `
+                <tr>
+                 <td>${spot.name}</td>
+                 <td>${spot.description}</td>
+                 <td>
+                 <a href="${mapUrl}" target="_blank">View on Map</a>
+                 </td>
+                </tr>
+
+            `;
+
+            $('#top-spots-table tbody').append(row);
+        });
+    });
+ });
